2e608361696d430aef963e3e799d13f1dc7bd9b2
skincrawl <123931483+skincrawl@users.noreply.github.com>
2025-01-26 11:24:34 +0200

    Various changes
    Added the new music
    
    Made the music play in the narrative screen
    
    Removed dark fan sprite
    
    Enabled the fan particles

--- gamejam_2025_bubble/Scripts/globals.gd
+++ gamejam_2025_bubble/Scripts/globals.gd
@@ -1,4 +1,8 @@
 extends Node
 
+var audio_stream:AudioStream = preload("res://Assets/Music/MainMenuTheme_Ilman_Introa.mp3")
+
 var level:Level
 var bubbles:Bubbles
+
+var music_spot:float
--- gamejam_2025_bubble/Scripts/narative_screen.gd
+++ gamejam_2025_bubble/Scripts/narative_screen.gd
@@ -1,6 +1,11 @@
 extends Control
 
 
+func _ready() -> void:
+	
+	$music.stream = Globals.audio_stream
+	$music.play(Globals.music_spot)
+
 
 func _on_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
