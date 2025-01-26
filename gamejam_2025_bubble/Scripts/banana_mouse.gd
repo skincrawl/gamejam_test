dc13f607f77003eddd7fdbbad7b16651fe4cdcca
skincrawl <123931483+skincrawl@users.noreply.github.com>
2025-01-26 06:26:56 +0200

    Wind particles
    Added the animated title
    
    Added wind particles to the mouse

--- gamejam_2025_bubble/Scripts/banana_mouse.gd
+++ gamejam_2025_bubble/Scripts/banana_mouse.gd
@@ -9,9 +9,16 @@ var speed:float = 0.0
 
 func _process(_delta:float) -> void:
 	
+	
+	
 	if Input.is_action_pressed("blowing"):
+		$Part.look_at(Globals.bubbles.position)
+		$Part.rotation -= deg_to_rad(90.0)
+		$Part.position = position
+		$Part/Node2D/CPUParticles2D.emitting = true
 		speed = MAX_ROT_SPEED
 	else:
 		speed = move_toward(speed, 0.0, SLOWDOWN_SPEED * _delta)
+		$Part/Node2D/CPUParticles2D.emitting = false
 	
 	rotation += deg_to_rad(speed)
--- gamejam_2025_bubble/Scripts/globals.gd
+++ gamejam_2025_bubble/Scripts/globals.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var level:Level
+var bubbles:Bubbles
